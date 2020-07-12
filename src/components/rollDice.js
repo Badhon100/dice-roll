b73a74f177db0d5ab0ba2f71571fde03d8dc7377
Badhon100 <badhonx102@gmail.com>
2020-07-13 00:43:37 +0600

    Final commit

--- src/components/rollDice.js
+++ src/components/rollDice.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Die from './die';
-
+import '../css/rollDice.css';
 class RollDice extends Component {
 
     static defaultProps = {
@@ -10,23 +10,26 @@ class RollDice extends Component {
     constructor(props){
         super(props);
         this.state = { 
-            die1: "one"
+            die1: "one", rolling: false
         }
         this.roll = this.roll.bind(this);
         
     }
     roll(){
         const newDie = this.props.sides[Math.floor(Math.random() * this.props.sides.length)]; 
-        this.setState({die1: newDie}); 
+        this.setState({die1: newDie, rolling: true}); 
+        setTimeout(()=>{
+            this.setState({rolling: false});
+        }, 1000);
         
     }
 
     render() { 
         return ( 
-            <div>
+            <div className = "RollDice">
                 <Die face = {this.state.die1} />
                 
-                <button onClick = {this.roll}>Roll Dice</button>
+                <button onClick = {this.roll} disabled = {this.state.rolling}><b>{this.state.rolling ? "Rolling..." : "Roll Dice"}</b></button>
             </div>
          );
     }
